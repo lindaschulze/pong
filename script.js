545c9e3598426f9dca6546dba30f3187079a2553
lindaschulze <linda.schulze@mail.com>
2025-01-25 14:09:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 
 // Spielvariablen
-let mioY = canvas.height / 2 - 50;
-let mikaY = canvas.height / 2 - 50;
+let player1Y = canvas.height / 2 - 50;
+let player2Y = canvas.height / 2 - 50;
 let ballX = canvas.width / 2;
 let ballY = canvas.height / 2;
 let ballSpeedX = 5;
 let ballSpeedY = 5;
-let mioScore = 0;
-let mikaScore = 0;
+let player1Score = 0;
+let player2Score = 0;
 const paddleHeight = 100;
 const paddleWidth = 20;
 
@@ -33,12 +33,12 @@ canvas.addEventListener('touchmove', function (e) {
     let touchMoveY1 = e.touches[0].clientY;
     let deltaY1 = touchMoveY1 - touchStartY1; // Differenz der Bewegungen
 
-    mioY += deltaY1; // Schläger von Mio bewegen
+    player1Y += deltaY1; // Schläger von Mio bewegen
     touchStartY1 = touchMoveY1; // Position aktualisieren
 
     // Begrenzung der Bewegung des Schlägers
-    if (mioY < 0) mioY = 0;
-    if (mioY > canvas.height - paddleHeight) mioY = canvas.height - paddleHeight;
+    if (player1Y < 0) player1Y = 0;
+    if (player1Y > canvas.height - paddleHeight) player1Y = canvas.height - paddleHeight;
 }, false);
 
 // Touch-Event Listener für den rechten Schläger (Mika)
@@ -51,12 +51,12 @@ canvas.addEventListener('touchmove', function (e) {
     let touchMoveY2 = e.touches[1].clientY;
     let deltaY2 = touchMoveY2 - touchStartY2; // Differenz der Bewegungen
 
-    mikaY += deltaY2; // Schläger von Mika bewegen
+    player2Y += deltaY2; // Schläger von Mika bewegen
     touchStartY2 = touchMoveY2; // Position aktualisieren
 
     // Begrenzung der Bewegung des Schlägers
-    if (mikaY < 0) mikaY = 0;
-    if (mikaY > canvas.height - paddleHeight) mikaY = canvas.height - paddleHeight;
+    if (player2Y < 0) player2Y = 0;
+    if (player2Y > canvas.height - paddleHeight) player2Y = canvas.height - paddleHeight;
 }, false);
 
 // Ball-Bewegung und Kollisionserkennung
@@ -70,13 +70,13 @@ function updateBall() {
     }
 
     // Ball-Kollision mit den Schlägern
-    if (ballX < paddleWidth && ballY > mioY && ballY < mioY + paddleHeight) {
+    if (ballX < paddleWidth && ballY > player1Y && ballY < player1Y + paddleHeight) {
         ballSpeedX = -ballSpeedX;
-        mioScore++;
+        player1Score++;
     }
-    if (ballX > canvas.width - paddleWidth && ballY > mikaY && ballY < mikaY + paddleHeight) {
+    if (ballX > canvas.width - paddleWidth && ballY > player2Y && ballY < player2Y + paddleHeight) {
         ballSpeedX = -ballSpeedX;
-        mikaScore++;
+        player2Score++;
     }
 
     // Ball zurücksetzen, wenn er aus dem Spielfeld geht
@@ -93,5 +93,27 @@ function draw() {
 
     // Schläger für Mio und Mika
     ctx.fillStyle = "blue";
-    ctx.fillRect(0, mioY, paddleWidth, paddleHeight); // Mio's Schläger
-    ctx.fillRect(canvas.width - pa
+    ctx.fillRect(0, player1Y, paddleWidth, paddleHeight); // Mio's Schläger
+    ctx.fillRect(canvas.width - paddleWidth, player2Y, paddleWidth, paddleHeight); // Mika's Schläger
+
+    // Ball zeichnen
+    ctx.fillStyle = "red";
+    ctx.beginPath();
+    ctx.arc(ballX, ballY, 10, 0, Math.PI * 2);
+    ctx.fill();
+
+    // Punktestand anzeigen
+    ctx.fillStyle = "black";
+    ctx.font = "24px Arial";
+    ctx.fillText("Mio: " + player1Score, 20, 30);
+    ctx.fillText("Mika: " + player2Score, canvas.width - 120, 30);
+}
+
+// Spielaktualisierung
+function gameLoop() {
+    updateBall();
+    draw();
+    requestAnimationFrame(gameLoop);
+}
+
+gameLoop();  // Spiel starten
